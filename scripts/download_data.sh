54268c8889142c053c858385da07dae904065e18
zhenkirito123 <1784088659@qq.com>
2025-07-23 18:26:32 -0700

    fix downloading checkpoints

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -3,3 +3,6 @@ gdown --folder https://drive.google.com/drive/folders/1LDo6RMjJ5Wr2bmL7hdTavMAU5
 gdown --folder https://drive.google.com/drive/folders/1-pexA27aGR2U9ucEufPfzPV_BRLKvDPG?usp=drive_link
 gdown --folder https://drive.google.com/drive/folders/12bRrFYV24P6_f6Xbklr7h7f0CD7TRmXC?usp=drive_link
 gdown --folder https://drive.google.com/drive/folders/1tXrrP-ph8NBckYfjAriwKe_9Lybl718L?usp=drive_link
+
+cd ..
+gdown --folder https://drive.google.com/drive/folders/1pJkRpKfLoolybgVE5PplNH1rhQ3XOgTE?usp=drive_link
